8aad252b5d6bb77956ed1d5713d68626ff92fc18
Andrey Ognev <enquence@yandex.ru>
2023-07-11 01:21:59 +0300

    feature: добавить функцию удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -47,6 +47,7 @@ const createCardElement = (name, link) => {
   cardElement.querySelector('.card__image').src = link
   cardElement.querySelector('.card__image').alt = name
   cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => evt.target.classList.toggle('card__like-button_active'))
+  cardElement.querySelector('.card__trash-button').addEventListener('click', (evt) => evt.target.closest('.card').remove())
   return cardElement
 }
 
