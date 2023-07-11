96e4111ef492bfbf826b6d5b317658d5b0fc8010
Andrey Ognev <enquence@yandex.ru>
2023-07-12 02:39:18 +0300

    feature: добавить плавную анимацию открытия попапа

--- scripts/index.js
+++ scripts/index.js
@@ -25,9 +25,9 @@ const initialCards = [
   }
 ];
 
-const popupElement = document.querySelector('.popup')
-const popupContainerProfileElement = document.querySelector('.popup__container_type_profile')
-const popupContainerNewCardElement = document.querySelector('.popup__container_type_new-card')
+const popupProfileElement = document.querySelector('.popup_type_profile')
+const popupNewCardElement = document.querySelector('.popup_type_new-card')
+const popupImageElement = document.querySelector('.popup_type_image')
 const profileFormElement = document.querySelector('.form_type_profile')
 const addCardFormElement = document.querySelector('.form_type_new-card')
 const nameInput = document.querySelector('.form__field_type_name')
@@ -36,16 +36,26 @@ const nameElement = document.querySelector('.profile__user-name')
 const jobElement = document.querySelector('.profile__user-occupation')
 const placeTitleInput = document.querySelector('.form__field_type_place-title')
 const placeLinkInput = document.querySelector('.form__field_type_place-link')
+const imageElement = document.querySelector('.popup__image')
+const imageCaptionElement = document.querySelector('.popup__image-caption')
 const editButton = document.querySelector('.profile__edit-button')
 const addButton = document.querySelector('.profile__add-button')
 const closeButtonsArray = Array.from(document.querySelectorAll('.popup__close-button'))
+
+const showPicturesPopup = (evt) => {
+  popupImageElement.classList.add('popup_opened')
+  imageElement.src = evt.target.src
+  imageCaptionElement.textContent = evt.target.alt
+}
 
 const createCardElement = (name, link) => {
   const cardTemplate = document.querySelector('#card').content
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
   cardElement.querySelector('.card__title').textContent = name
-  cardElement.querySelector('.card__image').src = link
-  cardElement.querySelector('.card__image').alt = name
+  const cardImage = cardElement.querySelector('.card__image')
+  cardImage.src = link
+  cardImage.alt = name
+  cardImage.addEventListener('click', showPicturesPopup)
   cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => evt.target.classList.toggle('card__like-button_active'))
   cardElement.querySelector('.card__trash-button').addEventListener('click', (evt) => evt.target.closest('.card').remove())
   return cardElement
@@ -60,35 +70,41 @@ const addCards = (append = true, ...cards) => {
 }
 
 const showProfilePopup = () => {
-  popupElement.classList.add('popup_opened')
-  popupContainerProfileElement.classList.add('popup__container_visible')
+  popupProfileElement.classList.add('popup_opened')
   nameInput.value = nameElement.textContent
   jobInput.value = jobElement.textContent
 }
 
 const showAddCardPopup = () => {
-  popupElement.classList.add('popup_opened')
-  popupContainerNewCardElement.classList.add('popup__container_visible')
+  popupNewCardElement.classList.add('popup_opened')
   placeTitleInput.value = '';
   placeLinkInput.value = '';
 }
 
 const closePopup = (evt) => {
-  popupElement.classList.remove('popup_opened')
-  evt.target.closest('.popup__container').classList.remove('popup__container_visible')
+  evt.target.closest('.popup').classList.remove('popup_opened')
 }
 
 const handleProfileFormSubmit = (evt) => {
   evt.preventDefault()
   nameElement.textContent = nameInput.value
   jobElement.textContent = jobInput.value
-  closePopup()
+  closePopup(evt)
 }
 
 const handleAddCardFormSubmit = (evt) => {
   evt.preventDefault()
-  addCards(false, {name: placeTitleInput.value, link: placeLinkInput.value})
-  closePopup()
+  if (!placeTitleInput.value) {
+    alert('У места должно быть название!')
+    return
+  }
+  const imageBuffer = new Image()
+  imageBuffer.src = placeLinkInput.value
+  imageBuffer.onload = () => {
+    addCards(false, {name: placeTitleInput.value, link: placeLinkInput.value})
+    closePopup(evt)
+  }
+  imageBuffer.onerror = () => alert('По этой ссылке нет картинки!')
 }
 
 addCards(true, ...initialCards)
