dc65f23b936030db0ff2703d9c4463594ffd474b
Andrey Ognev <enquence@yandex.ru>
2023-07-13 01:11:23 +0300

    hotfix: исправить по комментариям ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const popupProfileElement = document.querySelector('.popup_type_profile')
 const popupNewCardElement = document.querySelector('.popup_type_new-card')
 const popupImageElement = document.querySelector('.popup_type_image')
@@ -42,47 +15,47 @@ const editButton = document.querySelector('.profile__edit-button')
 const addButton = document.querySelector('.profile__add-button')
 const closeButtonsArray = Array.from(document.querySelectorAll('.popup__close-button'))
 
+const openPopup = (popupElement) => popupElement.classList.add('popup_opened')
+const handleLikeClick = (evt) => evt.target.classList.toggle('card__like-button_active')
+const handleCardDeleteClick = (evt) => evt.target.closest('.card').remove()
+const closePopup = (popupElement) => popupElement.classList.remove('popup_opened')
+
 const showPicturesPopup = (evt) => {
-  popupImageElement.classList.add('popup_opened')
+  openPopup(popupImageElement)
   imageElement.src = evt.target.src
   imageCaptionElement.textContent = evt.target.alt
 }
 
-const createCardElement = (name, link) => {
+const createCardElement = (card) => {
   const cardTemplate = document.querySelector('#card').content
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
-  cardElement.querySelector('.card__title').textContent = name
+  cardElement.querySelector('.card__title').textContent = card.name
   const cardImage = cardElement.querySelector('.card__image')
-  cardImage.src = link
-  cardImage.alt = name
+  cardImage.src = card.link
+  cardImage.alt = card.name
   cardImage.addEventListener('click', showPicturesPopup)
-  cardElement.querySelector('.card__like-button').addEventListener('click', (evt) => evt.target.classList.toggle('card__like-button_active'))
-  cardElement.querySelector('.card__trash-button').addEventListener('click', (evt) => evt.target.closest('.card').remove())
+  cardElement.querySelector('.card__like-button').addEventListener('click', handleLikeClick)
+  cardElement.querySelector('.card__trash-button').addEventListener('click', handleCardDeleteClick)
   return cardElement
 }
 
 const addCards = (append = true, ...cards) => {
   const elementSection = document.querySelector('.elements')
   cards.forEach( card => {
-    if (append) elementSection.append(createCardElement(card.name, card.link))
-      else elementSection.prepend(createCardElement(card.name, card.link))
+    if (append) elementSection.append(createCardElement(card))
+      else elementSection.prepend(createCardElement(card))
   })
 }
 
 const showProfilePopup = () => {
-  popupProfileElement.classList.add('popup_opened')
+  openPopup(popupProfileElement)
   nameInput.value = nameElement.textContent
   jobInput.value = jobElement.textContent
 }
 
 const showAddCardPopup = () => {
-  popupNewCardElement.classList.add('popup_opened')
-  placeTitleInput.value = '';
-  placeLinkInput.value = '';
-}
-
-const closePopup = (evt) => {
-  evt.target.closest('.popup').classList.remove('popup_opened')
+  addCardFormElement.reset()
+  openPopup(popupNewCardElement)
 }
 
 const handleProfileFormSubmit = (evt) => {
@@ -113,4 +86,4 @@ profileFormElement.addEventListener('submit', handleProfileFormSubmit)
 addCardFormElement.addEventListener('submit', handleAddCardFormSubmit)
 editButton.addEventListener('click', showProfilePopup)
 addButton.addEventListener('click', showAddCardPopup)
-closeButtonsArray.forEach( (button) => button.addEventListener('click', closePopup) )
+closeButtonsArray.forEach( (button) => button.addEventListener('click', (evt) => closePopup(evt.target.closest('.popup'))) )
